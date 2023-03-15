52176008bc71bd29d69808fc8a59d7aa2baf569c
vaupunkt <119793693+vaupunkt@users.noreply.github.com>
2023-03-15 22:59:54 +0100

    Exercise repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+let word = "";
+var number;
 
+const repeatString = function (word, number) {
+    if (number < 0) return "ERROR";
+    let output = "";
+    for (let i = 0; i < number; i++) { 
+        output += word;
+    }
+    return output;
 };
 
 // Do not edit below this line
