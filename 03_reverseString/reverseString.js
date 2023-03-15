0717006713445a2e417e6f894047a9f4241d6e1f
vaupunkt <119793693+vaupunkt@users.noreply.github.com>
2023-03-15 23:26:47 +0100

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const length = string.length;
+    let output = "";
+    for (let index = 0; index < length; index++) {
+        let element = string[length - index -1];
+        output += element;
+    }
+    return output.toLowerCase();
 };
 
 // Do not edit below this line
